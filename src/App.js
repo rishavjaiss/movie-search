416e87f5cb814bb64a473283ca933351fdcb161f
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-05-20 02:07:12 +0530

    UI Modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import SearchMovie from "./components/SearchMovie/SearchMovie";
 
 function App() {
-  useEffect(() => {
-    document.title = "Movie App";
-  }, []);
-
   return (
     <div className="container">
       <h1 className="title">Search Movies!</h1>
