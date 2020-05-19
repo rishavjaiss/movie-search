2631c34aa91d932ad090ea34bacb63b6080ab1e7
Rishav Jaiswal <rishavjaiss14@gmail.com>
2020-05-20 00:57:58 +0530

    Updates

---
+++ precache-manifest.88b3d522c4d4a7a6056403806bb7be89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2565ed8714924dfa1d9def3e6690774",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "7f22f18a94e19e040ff6",
+    "url": "/movie-search/static/css/main.ec359e31.chunk.css"
+  },
+  {
+    "revision": "6ad631a0098276f46988",
+    "url": "/movie-search/static/js/2.bc7b4226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.bc7b4226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f22f18a94e19e040ff6",
+    "url": "/movie-search/static/js/main.3910aac7.chunk.js"
+  },
+  {
+    "revision": "eb7c7aee420f65e28f7e",
+    "url": "/movie-search/static/js/runtime-main.8a6e7700.js"
+  }
+]);
